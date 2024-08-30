1fc8d627a175cd6822f74299cbf3828b4d022b89
MunSun <msunchalyaev@gmail.com>
2024-08-30 11:33:48 +0300

    init service

---
+++ src/main/java/com/munsun/vacation_pay_service/VacationPayServiceApplication.java
@@ -0,0 +1,11 @@
+package com.munsun.vacation_pay_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPayServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPayServiceApplication.class, args);
+	}
+}
