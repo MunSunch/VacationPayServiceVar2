6d03d82a5432f78575a7856bd6eaaad914221777
MunSun <msunchalyaev@gmail.com>
2024-09-01 09:54:12 +0300

    add unit tests

--- src/main/java/com/munsun/vacation_pay_service/controllers/impl/VacationController.java
+++ src/main/java/com/munsun/vacation_pay_service/controllers/impl/VacationController.java
@@ -3,14 +3,8 @@ package com.munsun.vacation_pay_service.controllers.impl;
 import com.munsun.vacation_pay_service.controllers.VacationApi;
 import com.munsun.vacation_pay_service.services.VacationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @RestController
--- src/test/java/com/munsun/vacation_pay_service/services/VacationServiceUnitTests.java
+++ src/test/java/com/munsun/vacation_pay_service/services/VacationServiceUnitTests.java
@@ -1,10 +1,13 @@
 package com.munsun.vacation_pay_service.services;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class VacationServiceUnitTests {
@@ -13,7 +16,15 @@ public class VacationServiceUnitTests {
 
     @DisplayName("Test calculate pay")
     @Test
-    public void given() {
+    public void given25DaysAnd22000Salary_whenCalculatePay_thenShouldCalculatePay() {
+        BigDecimal salary = new BigDecimal("22000");
+        int days = 25;
+        BigDecimal expected = new BigDecimal("18454.00");
 
+        BigDecimal actual = vacationService.calculate(salary, days);
+
+        assertThat(actual)
+                .isNotNull()
+                .isEqualTo(expected);
     }
 }
