fea36faf94114bb4823e3af3d2a9b06d70ecf77f
MunSun <msunchalyaev@gmail.com>
2024-09-03 14:00:29 +0300

    remove openfeign package

--- src/main/java/com/munsun/vacation_pay_service/VacationPayServiceApplication.java
+++ src/main/java/com/munsun/vacation_pay_service/VacationPayServiceApplication.java
@@ -2,7 +2,6 @@ package com.munsun.vacation_pay_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
