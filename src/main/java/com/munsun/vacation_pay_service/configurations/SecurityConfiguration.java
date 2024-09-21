db029405765198d36ff7ae566a756acf666e35d5
MunSun <msunchalyaev@gmail.com>
2024-09-21 16:51:35 +0300

    add security basic configuration /actuator/**

---
+++ src/main/java/com/munsun/vacation_pay_service/configurations/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.munsun.vacation_pay_service.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeRequests(requests ->
+                        requests.mvcMatchers("/actuator/**").hasRole("ADMIN")
+                                .anyRequest().permitAll())
+                .httpBasic()
+                .and().build();
+    }
+}
